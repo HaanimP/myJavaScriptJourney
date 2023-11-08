489dcaca4a8e0d8d959865595c953e435a8b46c1
HaanimP <haanimpietersen@gmail.com>
2023-11-08 10:29:42 +0200

    added variables and opearators

---
+++ index.js
@@ -0,0 +1,15 @@
+let language = 'JavaScript';
+console.log(language)
+
+let score = '10';
+console.log(score)
+
+let length = '10';
+console.log(length)
+
+let width = '5';
+console.log(width)
+
+let area = 10 * 5
+console.log(area)
+console.log(`the area of the rectangle is ${length * width} where it was calculated `);
